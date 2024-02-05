76abb250758ae9fb22a472cf01314b8caa10d10c
zaiem <zaiem.180410116045@gmail.com>
2024-02-05 09:38:02 -0500

    update(category): added loop

--- src/app/pages/create-event/create-event.component.ts
+++ src/app/pages/create-event/create-event.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CATEGORIES } from 'src/constants/constants';
 
 @Component({
   selector: 'app-create-event',
   templateUrl: './create-event.component.html',
-  styleUrls: ['./create-event.component.css']
+  styleUrls: ['./create-event.component.css'],
 })
-export class CreateEventComponent {
+export class CreateEventComponent implements OnInit {
+  categories: string[] = [];
 
+  ngOnInit(): void {
+    this.categories = CATEGORIES;
+  }
 }
